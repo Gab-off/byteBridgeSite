f235f58c9668091565858c471b6c00ae828d46a4
Gabriel de Miranda Albuquerque <albuquerque1.gabriel@gmail.com>
2025-10-23 18:14:12 -0300

    Refactor of archives

--- scripts/mobileMenu.js
+++ scripts/mobileMenu.js
@@ -3,41 +3,39 @@ const btnMenu = document.querySelector(".btn-menu");
 const menu = document.querySelector(".menu");
 
 function handleButtonClick(event) {
-	if (event.type === "touchstart") event.preventDefault();
-	event.stopPropagation();
-	nav.classList.toggle("active");
-	handClickOutside(menu, () => {
-		nav.classList.remove("active");
-	});
+    if (event.type === "touchstart") event.preventDefault();
+    event.stopPropagation();
+    nav.classList.toggle("active");
+    setAria();
+    handleClickOutside(menu, () => {
+        nav.classList.remove("active");
+        setAria();
+    });
 }
 
 function handleClickOutside(targetElement, callback) {
-	const html = document.documentElement;
-	function handleHTMLClick(event) {
-		if (!targetElement.contains(event.target)) {
-			targetElement.removeAttribute("data-target");
-			html.removeEventListener("click", handleHTMLClick);
-			html.removeEventListener("touchstart", handleHTMLClick);
-			callback();
-		}
-	}
-
-	if (!targetElement.hasAttribute("data-target")) {
-		html.addEventListener("click", handleHTMLClick);
-		html.addEventListener("touchstart", handleHTMLClick);
-		targetElement.setAttribute("data-target", "");
-	}
+    const html = document.documentElement;
+    function handleHTMLClick(event) {
+        if (!targetElement.contains(event.target) && !btnMenu.contains(event.target)) {
+            targetElement.removeAttribute("data-target");
+            html.removeEventListener("click", handleHTMLClick);
+            html.removeEventListener("touchstart", handleHTMLClick);
+            callback();
+        }
+    }
+    if (!targetElement.hasAttribute("data-target")) {
+        html.addEventListener("click", handleHTMLClick);
+        html.addEventListener("touchstart", handleHTMLClick);
+        targetElement.setAttribute("data-target", "");
+    }
 }
 
 function setAria() {
-	const isActive = nav.classList.contains("active");
-	btnMenu.setAttribute("aria-expanded", isActive);
-	if (isActive) {
-		btnMenu.setAttribute("aria-label", "Fechar Menu");
-	} else {
-		btnMenu.setAttribute("aria-label", "Abrir Menu");
-	}
+    const isActive = nav.classList.contains("active");
+    btnMenu.setAttribute("aria-expanded", isActive);
+    btnMenu.setAttribute("aria-label", isActive ? "Fechar Menu" : "Abrir Menu");
 }
 
+// Corrija esta linha no HTML/SCSS: <ul class="nav-section_black menu" ...>
 btnMenu.addEventListener("click", handleButtonClick);
 btnMenu.addEventListener("touchstart", handleButtonClick);
